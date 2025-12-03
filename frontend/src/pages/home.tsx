1c4365c077a0e253cd1817156e621d0a90689878
Victor Cavalcanti <victor.mca.dev@gmail.com>
2025-12-03 12:26:45 -0300

    removidos itens de mídia não usados

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,20 +1,8 @@
 export default function Home() {
   return(
     <>
-      <h1>Iconoclasts</h1>
-      <iframe 
-        width="560" height="315" 
-        src="https://www.youtube.com/embed/SxCMFV4jzW4?si=kd1RC8Re7mKsrnJ3" 
-        title="YouTube video player" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      >
-      </iframe>
+      <h1>Meu Jogo</h1>
       <section id="texto-introducao" style={{ margin: "20px 20%" }}>
-        <h3>Um mundo de escassez</h3>
-        <p>Num futuro onde a humanidade já exauriu todos os recursos do planeta, foi descoberta uma nova forma de energia milagrosa chamada "Marfim", sob controle do império comandado pela religião do "Propósito Único".</p>
-        <h3>Onde ajudar o próximo é um crime capital</h3>
-        <p>Neste mundo, apenas engenheiros licenciados podem mexer em quaisquer máquinas, e nossa protagonista, Robin, trabalha arduamente para conseguir sua licença. Mas quando policiais do impédio descobrem que ela conserta máquinas de graça para a população, ela é acusada de heresia, e passa a ser perseguida pelo império.</p>
-        <p>Ajude Robin a conseguir sua liberdade e a proteger seus amigos em um mundo corrupto e perigoso.</p>
       </section>
     </>
   )
